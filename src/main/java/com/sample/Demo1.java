4d09573163eae4004e9c8cdc55badf3775770e76
prodipto79 <prodiptodutta4@gmail.com>
2020-10-10 20:41:26 +0530

    adding new code

--- src/main/java/com/sample/Demo1.java
+++ src/main/java/com/sample/Demo1.java
@@ -1,9 +1,37 @@
 package com.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo1 {
 
     // test file
+    public static void main(String[] args) {
 
+        List<Student> obj = new ArrayList<Student>();
+        String name;
+        String roll;
+        int age;
+
+        for(int i = 0;i<4;i++){
+
+            name = "Student name" + i;
+            roll = "roll" + i;
+            age = 12;
+            Student stu = new Student(name,roll,age);
+            obj.add(stu);
+            System.out.println(stu.toString());
+
+        }
+
+        for(Student stu : obj){
+            System.out.println(stu.getStuName());
+        }
+
+
+
+
+    }
 
 
 }
--- src/main/java/com/sample/Student.java
+++ src/main/java/com/sample/Student.java
@@ -2,7 +2,30 @@ package com.sample;
 
 public class Student {
 
-    string stuName;
-    
+    private String stuName;
+    private String roll;
+    private int age;
+
+
+    public Student(String stuName, String roll, int age ){
+
+        this.stuName = stuName;
+        this.roll = roll;
+        this.age = age;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
 
 }
